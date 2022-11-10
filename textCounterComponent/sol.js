ffd67716a530d73199698b321a67aad4e3165b27
shai shmoelov <shaishmoelov@gmail.com>
2022-11-10 13:05:26 +0200

    update

--- textCounterComponent/sol.js
+++ textCounterComponent/sol.js
@@ -1,29 +1,3 @@
 class textCounter{
-    constructor(el){
-        el.innerHTML = `
-        <div class="container">
-        <div class="counter"></div>
-        <textarea name="message"></textarea>
-        </div>
-        `
-        
-        let timerId = setInterval(this.check, 1000);
-        this.counter = el.querySelector(`.counter`);
-        this.root = el;
-        this.text = el.querySelector(`textarea`);
-    }
-
-    check = () =>{
-        let counter_ = this.text.textLength;
-        this.counter.innerHTML = counter_;
-    }
-
-
-}
-for(let i = 0; i < 10; i++){
-    const e1 = document.createElement('div');
-    document.body.appendChild(e1);
-    const c1 = new textCounter(e1);    
-
-}
     
+}
